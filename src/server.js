cacb007b5d72a41060f88a197329e7d7344d389d
ilhamtrz <ilhamtriza@gmail.com>
2021-12-10 10:35:24 +0700

    add save notes

--- src/server.js
+++ src/server.js
@@ -1,21 +1,17 @@
-/* eslint-disable eol-last */
-/* eslint-disable no-multiple-empty-lines */
-/* eslint-disable no-trailing-spaces */
-/* eslint-disable import/no-unresolved */
 const Hapi = require('@hapi/hapi');
- 
- 
+const routes = require('./routes');
+
 const init = async () => {
   const server = Hapi.server({
     port: 5000,
     host: 'localhost',
   });
- 
- 
+
+  server.route(routes);
+
   await server.start();
 
   console.log(`Server berjalan pada ${server.info.uri}`);
 };
- 
- 
+
 init();
