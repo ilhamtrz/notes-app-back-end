81f041638c2ab388412e2d557c3d47328a391077
ilhamtrz <ilhamtriza@gmail.com>
2021-12-10 09:41:56 +0700

    http server init

--- src/server.js
+++ src/server.js
@@ -1,2 +1,21 @@
-// eslint-disable-next-line no-console
-console.log('Hallo kita akan membuat RESTful API');
+/* eslint-disable eol-last */
+/* eslint-disable no-multiple-empty-lines */
+/* eslint-disable no-trailing-spaces */
+/* eslint-disable import/no-unresolved */
+const Hapi = require('@hapi/hapi');
+ 
+ 
+const init = async () => {
+  const server = Hapi.server({
+    port: 5000,
+    host: 'localhost',
+  });
+ 
+ 
+  await server.start();
+
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+ 
+ 
+init();
